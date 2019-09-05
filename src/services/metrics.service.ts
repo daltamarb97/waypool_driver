6eff8d9d4bdf80bcd456980dd438e90f71492ba7
Daniel Altamar <danielaltamarb@gmail.com>
2019-09-05 17:21:39 +0000

    metrics.service.ts created online with Bitbucket

---
+++ src/services/metrics.service.ts
@@ -0,0 +1,35 @@
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Injectable } from "@angular/core";
+
+import { AngularFireAuth } from "angularfire2/auth";
+
+@Injectable()
+export class MetricsService {
+    constructor(public afDB: AngularFireDatabase, public AngularFireAuth: AngularFireAuth){
+    }
+
+
+    public createdReserves(university, driverUid,keyReserve,driver,car,destination,origin,note,precio,startHour,typeOfReserve){
+    //send every reserve that were created
+    this.afDB.database.ref('allReservesCreated/'+university + '/'+driverUid+'/'+keyReserve).update({
+        driver: driver,
+        car:car,
+        destination:destination,
+        origin:origin,
+        note: note,
+        price:precio,
+        startHour:startHour,
+        type: typeOfReserve
+
+      });
+
+   }   
+   public tripsInitiated(university, driverUid,keyTrip,trip){
+    //send every trip that were initiated
+  this.afDB.database.ref('allTripsInitiated/'+university + '/'+driverUid+'/'+keyTrip).update(trip);
+
+    } 
+
+     }
+
+     
